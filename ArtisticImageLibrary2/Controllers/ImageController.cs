b744c5b672a7eea1a1be97ccf762a5241a2dc525
Syed Rahman Bokhari <rbokhari@gmail.com>
2020-09-20 16:45:28 -0400

    First push after completion

---
+++ ArtisticImageLibrary2/Controllers/ImageController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ArtisticImageLibrary2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private readonly ILogger<ImageController> logger;
+        public ImageController(ILogger<ImageController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet(Name = "GetImages")]
+        public IActionResult Get()
+        {
+            var path = @Path.Combine(Directory.GetCurrentDirectory(), "www", "attachments");
+            var files = Directory.GetFiles(path);
+            if (files!= null && files.Count() > 0)
+            {
+                var fileNames = files.Select(c => Path.GetFileName(c));
+                return Ok(fileNames);
+            }
+            return NoContent();
+        }
+
+
+        [HttpPost(Name = "postUpload")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Post(IFormFile file)
+        {
+            try
+            {
+                if (file == null || file.Length == 0)
+                    return Content("File not found");
+
+                var path = @Path.Combine(Directory.GetCurrentDirectory(), "www", "attachments", file.FileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                // logging error
+                logger.LogError(500, ex.Message);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{filename}", Name = "DeleteFile")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Delete(string filename)
+        {
+            try
+            {
+                var path = @Path.Combine(Directory.GetCurrentDirectory(), "www", "attachments", filename);
+
+                if (System.IO.File.Exists(path))
+                {
+                    System.IO.File.Delete(path);
+                    return NoContent();
+                }
+                throw new FileNotFoundException();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        private void CreateThumbNail(string path)
+        {
+            
+            Image image = Image.FromFile(path);
+            Image thumb = image.GetThumbnailImage(120, 120, () => false, IntPtr.Zero);
+            thumb.Save(Path.ChangeExtension(path, "thumb"));
+        }
+
+    }
+}
---
+++ ArtisticImageLibrary2/Startup.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace ArtisticImageLibrary2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddMvc(options =>
+            {
+                options.ReturnHttpNotAcceptable = true; // incase json accept header send, response json, incase xml accept header send then implement xml response
+                options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());   // response xml 
+                options.InputFormatters.Add(new XmlSerializerInputFormatter(options)); // request xml
+            })
+            .AddJsonOptions(
+                options =>
+                {
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                }
+            )
+            .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        /// <param name="loggerFactory"></param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyHeader();
+                builder.AllowAnyMethod();
+                builder.AllowAnyOrigin();
+            });
+
+            var fullPath = Path.GetFullPath(Path.Combine(env.ContentRootPath, "www"));
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(fullPath),
+                RequestPath = @"/images"
+            });
+
+            app.UseMvc();
+
+            var logger = loggerFactory.CreateLogger("Artistic Library");
+            logger.LogInformation("Application Started !");
+        }
+    }
+}
